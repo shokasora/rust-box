7cacbe80739ec81335b1f648900e27c454366d6f
shokasora <soraxa1010@gmail.com>
2026-02-18 23:06:35 +0900

    Defef

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
-use crate::List::{Cons, Nil};
+use std::ops::Deref;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
+fn hello(name: &str) {
+    println!("Hello {name}");
+}
 fn main() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+    let x = 5;
+    let y = MyBox::new(x);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
